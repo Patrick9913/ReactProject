18ff2dcd64ac736947c90a3901291a551cad9aba
Patrick <patrickyoel13@gmail.com>
2023-11-28 13:07:02 -0300

    general changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Navbar } from './components/Navbar/Navbar'
+import { Products } from './components/products/Products'
 import './App.css'
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
       <Navbar />
+      <h1 className=' text-center text-3xl font-light my-7'>Productos Destacados</h1>
+      <Products />
     </>
   )
 }
