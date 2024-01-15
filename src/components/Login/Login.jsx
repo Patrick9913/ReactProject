d093f4d13c9f8ba615b63950fe7d414fda21f907
Patrick <patrickyoel13@gmail.com>
2024-01-15 20:27:07 -0300

    Style Update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -25,11 +25,11 @@ export const Login = () => {
 
     return (
         <div className=" mt-32 w-full flex justify-center">
-            <div className=" bg-sky-950 w-[500px] h-[500px] rounded-md p-4 flex flex-col items-center gap-4 justify-center">
-                <p className=" text-stone-50">Bienvenido a CyberCart Haven</p>
-                <form className=" flex flex-col gap-4 justify-center items-center" onSubmit={handleSubmit}>
+            <div className="bg-slate-800 w-[500px] h-[500px] rounded-md p-4 flex flex-col items-center gap-4 justify-center">
+                <p className=" text-stone-50 mb-5">Bienvenido a CyberCart Haven</p>
+                <form className=" flex flex-col gap-8 justify-center items-center" onSubmit={handleSubmit}>
                     <input 
-                        className="border-2 rounded-md w-72 p-3"
+                        className="border border-black w-72 p-3"
                         placeholder="Email"
                         type="text" 
                         value={values.email}
@@ -37,18 +37,19 @@ export const Login = () => {
                         name="email"
                     />
                     <input 
-                        className="border-2 rounded-md w-72 p-3"
+                        className="border border-black w-72 p-3"
                         placeholder="Contraseña"
                         type="password" 
                         value={values.password}
                         onChange={handleInputChange}
                         name="password"
                     />
-                    <button type="submit" className=" rounded-md w-36 p-4 text-stone-50 border-2">
+                    <button type="submit" className="rounded-md w-36 p-4 text-stone-50 border hover:bg-white hover:text-black ease-in duration-150">
                         Iniciar Sesion
                     </button>
                 </form>
-                <button className="rounded-md p-4 w-36 text-stone-50 border-2" onClick={() => register(values)}>
+                <button onClick={googleLogin} className="rounded-md w-64 p-4 text-stone-50 border hover:bg-white hover:text-black ease-in duration-150">Iniciar Sesion con Google</button>
+                <button className="rounded-md p-4 w-36 text-stone-50 border hover:bg-white hover:text-black ease-in duration-150" onClick={() => register(values)}>
                     Registar
                 </button>
             </div>
